eb35293be23b775b376d92f490ff601c848961f5
xmoutaz <xmoutaz@hotmail.com>
2021-07-08 21:56:59 +0100

    coding challenge sensory minds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SquareComponent } from './components/square/square.component';
-import { BoardComponent } from './components/board/board.component';
 import { BingoBoardComponent } from './components/bingo-board/bingo-board.component';
 import { FormComponent } from './components/form/form.component';
 import { FormsModule } from '@angular/forms';
@@ -12,8 +10,6 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    SquareComponent,
-    BoardComponent,
     BingoBoardComponent,
     FormComponent,
   ],
