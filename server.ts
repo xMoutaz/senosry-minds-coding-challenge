977174a8c49d8f585e737fd0b5eaa941f6e1db61
xmoutaz <xmoutaz@hotmail.com>
2021-07-08 15:31:42 +0100

    adding heroku config

--- server.ts
+++ server.ts
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + '/dist/fresh-angular'));
 app.get('/*', function(req,res) {
    res.sendFile(path.join(__dirname+'/dist/fresh-angular/index.html'));
 });
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200, () => console.log('Server started at port: '+ 4200));
